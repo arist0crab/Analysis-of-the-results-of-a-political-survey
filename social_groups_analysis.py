6e90c57a5651d811a8005b0b5ac04ebd041ae6ab
Varvara Bondareva <BondareVarechka@yandex.ru>
2026-04-21 23:42:32 +0300

    in-depth group analyzer has been added, operating instructions have been added to README.md

---
+++ social_groups_analysis.py
@@ -0,0 +1,108 @@
+from translaters import *
+from polytology_data_analys.parse_primary_json_data import get_json_data, create_base_empty_dict, load_blank_to_json
+
+
+choose_group_menu = """
+Выберите признак группы респондентов, по которому необходимо
+провести анализ:
+0. Возраст
+1. Место жительства
+2. Род деятельности
+3. Обход блокировок
+4. Заблокированные сервисы
+5. Динамика изменения политических взглядов
+6. Динамика изменения уровня технических навыков
+7. Субъективная оценка эффективности блокировок
+8. Отношение к публикации своего мнения в медиа
+9. Выход
+"""
+
+
+def get_raw_group_analysis(survey_data_filename):
+    menu_option = choose_group_for_analysis()
+    raw_group_analysis_json = get_data_for_special_group(menu_option, survey_data_filename)
+    return raw_group_analysis_json
+
+
+def get_processed_group_analysis(survey_data_filename):
+    menu_option = choose_group_for_analysis()
+    processed_group_analysis_json = get_statistics_for_special_group(menu_option, survey_data_filename)
+    return processed_group_analysis_json
+
+
+def get_statistics_for_special_group(group_index, survey_data_filename):
+    special_json_data = get_data_for_special_group(group_index, survey_data_filename)
+
+    for current_group in special_json_data.keys():
+        current_group_json = special_json_data[current_group]
+        group_respondents_quantity = calculate_group_respondents(current_group_json)
+
+        for group_characteristic in current_group_json.keys():
+            current_group_characteristic_json = current_group_json[group_characteristic]
+
+            for group_characteristic_item in current_group_characteristic_json.keys():
+                current_percentage = current_group_characteristic_json[group_characteristic_item] / group_respondents_quantity * 100
+                current_group_characteristic_json[group_characteristic_item] = f"{current_percentage:.2f}%"
+        add_respondents_quantity_to_group(current_group_json, group_respondents_quantity)
+
+    return special_json_data
+
+
+def get_data_for_special_group(group_index, survey_data_filename):
+
+    json_data = dict()
+    survey_data = get_json_data(survey_data_filename)
+    key_translators = get_key_translators()
+
+    if group_index < 9:
+
+        analysis_key = key_translators[group_index]
+        json_data = {key: create_base_empty_dict() for key in analysis_key.keys()}
+
+        for current_group in analysis_key.keys():
+            survey_group_name = analysis_key[current_group]
+
+            for blank in survey_data:
+                for question in blank:
+                    if question[1] == survey_group_name:
+                        json_data[current_group] = load_blank_to_json(json_data[current_group], blank)
+
+    return  json_data
+
+
+def calculate_group_respondents(json_group_data):
+    return sum(json_group_data["age"].values())
+
+
+def add_respondents_quantity_to_group(json_group_data, respondents_quantity):
+    json_group_data["respondents_quantity"] = respondents_quantity
+
+
+def get_key_translators():
+    return (
+        age_translater,
+        place_of_residence_translater,
+        profession_translater,
+        frequency_of_using_bypass_translater,
+        blocked_services_using_translater,
+        political_activity_dynamic_translater,
+        technical_skills_dynamic_translater,
+        blocks_efficiency_opinion_translater,
+        publish_opinion_in_media_translater
+    )
+
+
+def choose_group_for_analysis():
+    option_was_inputted = False
+    menu_option = 9
+
+    while not option_was_inputted:
+        print(choose_group_menu)
+        try:
+            menu_option = int(input("Номер опции: "))
+            option_was_inputted = (0 <= menu_option <= 9)
+
+        except ValueError:
+            print("Введите только число - номер опции меню")
+
+    return menu_option
---
+++ translaters.py
@@ -0,0 +1,78 @@
+age_translater = {
+    "less 18": "Младше 18 лет",
+    "18-25": "18-25 лет",
+    "26-40": "26-40 лет",
+    "41-60": "41-60 лет",
+    "60 and elder": "Старше 60 лет"
+}
+
+place_of_residence_translater = {
+    "Moscow": "Москва",
+    "Moscow area": "Московская область",
+    "Saint-Petersburg": "Санкт-Петербург",
+    "another million citizens town": "Другой город-миллионник",
+    "another less than million citizens town": "Другой город до 1млн жителей",
+    "settlement or village": "Поселок или село"
+}
+
+profession_translater = {
+    "schoolboy": "Школьник",
+    "technical student": "Студент технической специальности",
+    "humanities student": "Студент гуманитарной / естественнонаучной / творческой специальности",
+    "IT-sector worker": "Работник IT-сферы / инженерии",
+    "not IT-sector worker": "Работник в сфере, не относящийся к IT / инженерии",
+    "entrepreneur": "Предприниматель",
+    "budgetary sphere": "Бюджетная сфера",
+    "pensioner": "Пенсионер",
+    "another": "Другое"
+}
+
+frequency_of_using_bypass_translater = {
+    "yes regularly": "Да, регулярно",
+    "yes but rarely": "Да, но редко",
+    "no but i know": "Нет, но знаю, что это такое ",
+    "no and i dont know": "Нет и не знаю, что это такое"
+}
+
+blocked_services_using_translater = {
+    "youtube": "YouTube",
+    "telegram": "Telegram",
+    "instagram": "Instagram (запрещен на территории РФ)",
+    "facebook": "Facebook (запрещен на территории РФ)",
+    "tik-tok": "TikTok",
+    "twitter/x": "Twitter / X",
+    "discord": "Discord",
+    "foreign-news": "Зарубежные новостные сайты",
+    "foreign-music": "Зарубежные музыкальные сервисы",
+    "another": "Другое",
+    "i dont use": "Не пользуюсь такими"
+}
+
+political_activity_dynamic_translater = {
+    "became more opposite cuz of blocks": "Да, стали более оппозиционными - в том числе из-за того, что блокировку интернет-ресурсов я воспринимаю как давление на свободу информации",
+    "became more opposite independence of blocks": "Да, стали более оппозиционными. Причины не связаны с интернет-блокировками",
+    "became more loyal cuz of blocks": "Да, стали более лояльными к власти: я считаю блокировки оправданной мерой для безопасности",
+    "became more loyal independence of blocks": "Да, стали более лояльными к власти. Причины не связаны с интернет-блокировками",
+    "have not changed": "Нет, мои взгляды не изменились",
+    "difficult to answer": "Затрудняюсь ответить"
+}
+
+technical_skills_dynamic_translater = {
+    "increased significantly": "Существенно вырос - до блокировок я почти ничего не умел, теперь разбираюсь хорошо",
+    "increased a little bit": "Немного вырос - раньше не сталкивался, теперь научился базовым вещам",
+    "has not changed: they were good": "Не изменился - я хорошо разбирался в этом еще до блокировок",
+    "has not changed: they were bad": "Не изменился - я до сих пор в этом ничего не понимаю",
+    "i dont use blocked resources": "Блокировки меня не коснулись, так как я не пользуюсь заблокированными ресурсами"
+}
+
+blocks_efficiency_opinion_translater = {
+    "effectively": "Эффективны - люди перестают пользоваться заблокированными сервисами",
+    "partially effectively": "Частично эффективны - некоторые люди перестают пользоваться привычными сервисами, другие находят пути обхода",
+    "not effectively": "Неэффективны - люди массово обходят блокировки и продолжают пользоваться запрещенными ресурсами"
+}
+
+publish_opinion_in_media_translater = {
+    "became carefully": "Да, я стал осторожнее",
+    "write all i want": "Нет, пишу все, что считаю нужным",
+    "never say opinion in media": "Я никогда не выражал мнение публично в соцсетях"
+}
